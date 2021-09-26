ad54ccdfa5324f0f05eab74139fbd3beceab3528
perdanaph <71249930+perdanaph@users.noreply.github.com>
2021-09-26 16:01:33 +0700

    tinggal tangan dan kaki

--- grafkom.py
+++ grafkom.py
@@ -2,38 +2,115 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-x = 1000
-y = 1000
+x = 500
+y = 500
 
 def pointsleft():
-    glPointSize(10)
+    glPointSize(5)
     glBegin(GL_POINTS)
     glColor3ub(34, 227, 169)
-    glVertex2f(8, 18)
-    glVertex2f(9, 17)
-    glVertex2f(10, 16)
-    glVertex2f(11, 15)
-    glVertex2f(12, 14)
-    glVertex2f(13, 13)
+    glVertex2f(10, 18)
+    glVertex2f(9.5, 18.5)
+    glVertex2f(9, 19)
+    glVertex2f(8.5, 19.5)
+    glVertex2f(8, 20)
+    glVertex2f(7.5, 20.5)
     glEnd()
 
 def pointrights():
-    glPointSize(10)
+    glPointSize(5)
     glBegin(GL_POINTS)
     glColor3ub(34, 227, 169)
-    glVertex2f(18, 13)
-    glVertex2f(19, 14)
-    glVertex2f(20, 15)
-    glVertex2f(21, 16)
-    glVertex2f(22, 17)
-    glVertex2f(23, 18)
+    glVertex2f(14, 18)
+    glVertex2f(14.5, 18.5)
+    glVertex2f(15, 19)
+    glVertex2f(15.5, 19.5)
+    glVertex2f(16, 20)
+    glVertex2f(16.5, 20.5)
+    glEnd()
+
+def badan():
+    glBegin(GL_POLYGON)
+    glColor3ub(34, 227, 169)
+    glVertex2f(10, 18)
+    glVertex2f(8, 16)
+    glVertex2f(8, 8)
+    glVertex2f(10,6)
+    glVertex2f(14,6)
+    glVertex2f(16, 8)
+    glVertex2f(16,16)
+    glVertex2f(14,18)
+    glEnd()
+
+# MATA
+def bolamata():
+    glPointSize(25)
+    glBegin(GL_POINTS)
+    glColor3ub(0,0,0)
+    glVertex2f(12,14.5)
+    glEnd()
+
+def retina():
+    glPointSize(50)
+    glBegin(GL_POINTS)
+    glColor3ub(56, 168, 50)
+    glVertex2f(12,14.5)
+    glEnd()
+
+def TB_kelopak_mata():
+    glBegin(GL_QUADS)
+    glColor3ub(112, 30, 2)
+    glVertex2f(10.5, 12.5) 
+    glVertex2f(13.5, 12.5) 
+    glVertex2f(13.5, 17) 
+    glVertex2f(10.5, 17) 
+    glEnd()
+
+def RL_kelopak_mata():
+    glBegin(GL_QUADS)
+    glColor3ub(112, 30, 2)
+    glVertex2f(10, 13) 
+    glVertex2f(14, 13) 
+    glVertex2f(14, 16) 
+    glVertex2f(10, 16)
+    glEnd()
+
+def mulut():
+    glBegin(GL_QUADS)
+    glColor3ub(255, 255, 255)
+    glVertex2f(10, 10)
+    glVertex2f(14, 10)
+    glVertex2f(14, 11)
+    glVertex2f(10, 11)
+    glEnd()
+
+def mulut2():
+    glBegin(GL_POLYGON)
+    glColor3ub(255, 18, 18)
+    glVertex2f(10, 8.5)
+    glVertex2f(10, 10)
+    glVertex2f(14, 10)
+    glVertex2f(14, 8.5)
+    glVertex2f(13.5, 8)
+    glVertex2f(10.5, 8)
+    glEnd()
+
+def mulut3():
+    glBegin(GL_POLYGON)
+    glColor3ub(209, 84, 84)
+    glVertex2f(10, 8.5)
+    glVertex2f(10.5, 9)
+    glVertex2f(13.5, 9)
+    glVertex2f(14, 8.5)
+    glVertex2f(13.5, 8)
+    glVertex2f(10.5, 8)
     glEnd()
 
 def iterate():
     glViewport(100, 100, 1000, 1000) # untuk mengatur area pandang
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    glOrtho(0.0, 100, 0.0, 100, 0.0, 1.0) # untuk mengatur berapa blok yang digunakan (skala) nilai x, y, z
+    glOrtho(0.0, 50, 0.0, 50, 0.0, 1.0) # untuk mengatur berapa blok yang digunakan (skala) nilai x, y, z
     glMatrixMode (GL_MODELVIEW)
     glLoadIdentity()
 
@@ -43,6 +120,14 @@ def showScreen():
     iterate()
     pointsleft()
     pointrights()
+    badan()
+    RL_kelopak_mata()
+    TB_kelopak_mata()
+    retina()
+    bolamata()
+    mulut()
+    mulut2()
+    mulut3()   
     glutSwapBuffers() # untuk membersihkan layar, double buffering
 
 glutInit()
